fa19d755a2a713412074bf7181806e75b94d1012
DeallerOdessa <Ashot.koro4e@gmail.com>
2025-06-12 00:52:22 +0300

    First commit

--- Aducation.py
+++ Aducation.py
@@ -1,18 +1,17 @@
-num = int(input("Enter a five-digit number: "))
+a = float(input("enter first value: "))
+b = float(input("enter second value: "))
+c = input("enter action: (+ - * /): " )
+if c == "+" :
+    print("Result : ", a+b)
+elif c == "-" :
+    print("Result : ", a-b)
+elif c == "*" :
+    print("Result : ", a*b)
+elif c == "/" :
+    if b == 0 :
+        print("Division by 0 is impossible")
+    else:
+        print("Result : ", a/b)
+else:
+    print("Invalid value! Choose from list (+ - * /)")
 
-n1 = num % 10
-num = num // 10
-
-n2 = num % 10
-num = num // 10
-
-n3 = num % 10
-num = num // 10
-
-n4 = num % 10
-num = num // 10
-
-n5 = num % 10
-new_number = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5
-
-print("result:", new_number)
